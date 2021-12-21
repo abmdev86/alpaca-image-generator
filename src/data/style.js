98d959498909392673768a64a7cc536b0c49dd92
Auroiah Morgan <amwebdev@outlook.com>
2021-12-21 13:01:15 -0500

    Update style.js

--- src/data/style.js
+++ src/data/style.js
@@ -1,8 +1,37 @@
-// import the images from the image folder
+// accessories
 import earings from '../img/alpaca/accessories/earings.png';
 import flower from '../img/alpaca/accessories/flower.png';
 import glasses from '../img/alpaca/accessories/glasses.png';
 import headphone from '../img/alpaca/accessories/headphone.png';
+
+//backgrounds
+import blue50 from '../img/alpaca/backgrounds/blue50.png';
+import blue60 from '../img/alpaca/backgrounds/blue60.png';
+import blue70 from '../img/alpaca/backgrounds/blue70.png';
+
+import darkBlue30 from '../img/alpaca/backgrounds/darkblue30.png';
+import darkBlue50 from '../img/alpaca/backgrounds/darkblue50.png';
+import darkBlue70 from '../img/alpaca/backgrounds/darkblue70.png';
+
+import green50 from '../img/alpaca/backgrounds/green50.png';
+import green60 from '../img/alpaca/backgrounds/green60.png';
+import green70 from '../img/alpaca/backgrounds/green70.png';
+
+import grey40 from '../img/alpaca/backgrounds/grey40.png';
+import grey70 from '../img/alpaca/backgrounds/grey70.png';
+import grey80 from '../img/alpaca/backgrounds/grey80.png';
+
+import red50 from '../img/alpaca/backgrounds/red50.png';
+import red60 from '../img/alpaca/backgrounds/red60.png';
+import red70 from '../img/alpaca/backgrounds/red70.png';
+
+import yellow50 from '../img/alpaca/backgrounds/yellow50.png';
+import yellow60 from '../img/alpaca/backgrounds/yellow60.png';
+import yellow70 from '../img/alpaca/backgrounds/yellow70.png';
+
+
+
+
 
 //  export the styleData object. It contains arrays for each category of style. Each array holds objects that represent each image. Each obj
 // has a name and an image source.
@@ -23,5 +52,16 @@ export const styleData = {
       name: 'Earings',
       img: earings
     }
+  ],
+  background: [
+    {
+      name: 'Blue50',
+      img: blue50
+    },
+    {
+      name: 'Blue60',
+      img: blue60
+    }
+
   ]
-}
+};
