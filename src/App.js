6b8a87208611794e4c7b4d647fa3300bb0bdfc64
Auroiah Morgan <amwebdev@outlook.com>
2021-12-21 13:01:04 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+import Alpaca from './components/Alpaca';
+import CategoryBtn from './components/CategoryBtn';
+import { styleData } from './data/style';
+
 
 function App() {
+  const [data, setData] = useState([]);
+  const [activeCategory, setActiveCategory] = useState("background");
+  const [activeStyle, setActiveStyle] = useState([]);
+  const [alpacaImg, setAlpacaImg] = useState([]);
+  const [styleOptions, setStyleOptions] = useState([]);
+
+  const [categoryIndex, setCategoryIndex] = useState(0);
+
+
+  //hooks
+  useEffect(() => {
+    const alpacaImageArray = [];
+    const defaultStylesArray = [];
+
+    Object.values(styleData).forEach(val => {
+      alpacaImageArray.push(val[0].img);
+      defaultStylesArray.push(val[0].name);
+    });
+    setAlpacaImg(alpacaImageArray);
+    setActiveStyle(defaultStylesArray);
+    setData(styleData);
+  }, []);
+
+  useEffect(() => {
+    const optionsArray = [];
+    styleData[activeCategory].map(item => {
+      return optionsArray.push(item);
+    });
+    setStyleOptions(optionsArray);
+  }, [activeCategory]);
+
+  // custom functions
+  function generateStylesWithOptions(category, index) {
+    // btn is set to active when selected
+    const currentName = category;
+    setActiveCategory(currentName);
+    setCategoryIndex(index);
+
+    // sets the state
+    const optionsArray = [];
+    styleData[category].map(item => {
+      return optionsArray.push(item);
+    });
+    setStyleOptions(optionsArray);
+  }
+
+  // const styleButtons = styleOptions.map((item, index) => {
+
+  // });
+
+  const alpacaAvatar = alpacaImg.map((item, index) => {
+    return (
+      <Alpaca pic={item} key={index} />
+    );
+  });
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Alpaca Generator</h1>
     </div>
   );
 }
