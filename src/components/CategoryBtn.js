2dc3df879a4079c3d382819c0404b277ced00198
Auroiah Morgan <amwebdev@outlook.com>
2021-12-21 13:01:11 -0500

    Create CategoryBtn.js

---
+++ src/components/CategoryBtn.js
@@ -0,0 +1,6 @@
+export default function CategoryBtn(props) {
+  const [activeCategory, name, generateStylesWithOptions] = [props.activeCategory, props.name, props.generateStylesWithOptions];
+  return (
+    <button className={activeCategory === name ? 'active' : ''} onClick={generateStylesWithOptions}>{name}</button>
+  );
+}
