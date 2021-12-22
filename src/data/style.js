4e32aa2babda5c29da1105377a48030f4f200fb4
Auroiah Morgan <amwebdev@outlook.com>
2021-12-22 10:49:53 -0500

    Update style.js

--- src/data/style.js
+++ src/data/style.js
@@ -29,14 +29,58 @@ import yellow50 from '../img/alpaca/backgrounds/yellow50.png';
 import yellow60 from '../img/alpaca/backgrounds/yellow60.png';
 import yellow70 from '../img/alpaca/backgrounds/yellow70.png';
 
+// Ears
+import defaultEars from '../img/alpaca/ears/default.png';
+import tiltedBackwards from '../img/alpaca/ears/tilt-backward.png';
+import tiltedForward from '../img/alpaca/ears/tilt-forward.png';
 
+// Eyes
+import defaultEyes from '../img/alpaca/eyes/default.png'
+import angryEyes from '../img/alpaca/eyes/angry.png'
+import naughtyEyes from '../img/alpaca/eyes/naughty.png'
+import pandaEyes from '../img/alpaca/eyes/panda.png'
+import smartEyes from '../img/alpaca/eyes/smart.png'
+import starEyes from '../img/alpaca/eyes/star.png'
+
+// Hair
+import bangHair from '../img/alpaca/hair/bang.png'
+import curls from '../img/alpaca/hair/curls.png'
+import defaultHair from '../img/alpaca/hair/default.png'
+import elegant from '../img/alpaca/hair/elegant.png'
+import fancy from '../img/alpaca/hair/fancy.png'
+import quiff from '../img/alpaca/hair/quiff.png'
+import short from '../img/alpaca/hair/short.png'
+
+// Leg
+import defaultLeg from '../img/alpaca/leg/default.png';
+import bubbleTea from '../img/alpaca/leg/bubble-tea.png';
+import cookie from '../img/alpaca/leg/cookie.png';
+import gameConsole from '../img/alpaca/leg/game-console.png';
+import tiltBackwardLeg from '../img/alpaca/leg/tilt-backward.png';
+import tiltForwardLeg from '../img/alpaca/leg/tilt-forward.png';
+
+// Mouth
+import astonished from '../img/alpaca/mouth/astonished.png'
+import defaultMouth from '../img/alpaca/mouth/default.png'
+import eating from '../img/alpaca/mouth/eating.png'
+import laugh from '../img/alpaca/mouth/laugh.png'
+import tongue from '../img/alpaca/mouth/tongue.png'
+
+// Neck
+import bendBackward from '../img/alpaca/neck/bend-backward.png'
+import bendForward from '../img/alpaca/neck/bend-forward.png'
+import defaultNeck from '../img/alpaca/neck/default.png'
+import thick from '../img/alpaca/neck/thick.png'
+
+// Nose
+import nose from '../img/alpaca/nose/nose.png'
 
 
 
 //  export the styleData object. It contains arrays for each category of style. Each array holds objects that represent each image. Each obj
 // has a name and an image source.
 export const styleData = {
-  accessories: [
+  Accessories: [
     {
       name: 'Headphone',
       img: headphone
@@ -53,7 +97,7 @@ export const styleData = {
       img: earings
     }
   ],
-  background: [
+  Background: [
     {
       name: 'Blue50',
       img: blue50
@@ -61,7 +105,190 @@ export const styleData = {
     {
       name: 'Blue60',
       img: blue60
+    }, {
+      name: 'Blue70',
+      img: blue70
+    },
+    {
+      name: 'DarkBlue30',
+      img: darkBlue30
+    },
+    {
+      name: 'DarkBlue50',
+      img: darkBlue50
+    }, {
+      name: 'DarkBlue70',
+      img: darkBlue70
+    }, {
+      name: 'Green50',
+      img: green50
+    },
+    {
+      name: 'Green60',
+      img: green60
+    },
+    {
+      name: 'Green70',
+      img: green70
+    }, {
+      name: 'Grey40',
+      img: grey40
+    },
+    {
+      name: 'Grey70',
+      img: grey70
+    }, {
+      name: 'Grey80',
+      img: grey80
+    }, {
+      name: 'Red50',
+      img: red50
+    },
+    {
+      name: 'Red60',
+      img: red60
+    }, {
+      name: 'Red70',
+      img: red70
+    }, {
+      name: 'Yellow50',
+      img: yellow50
+    }, {
+      name: 'Yellow60',
+      img: yellow60
+    }, {
+      name: 'Yellow70',
+      img: yellow70
+    },
+
+  ],
+  Ears: [
+    {
+      name: 'Default',
+      img: defaultEars
+    }, {
+      name: 'tiltForwards',
+      img: tiltedForward
+    }, {
+      name: 'tiltBackwards',
+      img: tiltedBackwards
+    }
+  ],
+  Eyes: [
+    {
+      name: 'Default',
+      img: defaultEyes
+    }, {
+      name: 'Angty',
+      img: angryEyes
+    }, {
+      name: 'Naughty',
+      img: naughtyEyes
+    }, {
+      name: 'Panda',
+      img: pandaEyes
+    }, {
+      name: 'Smart',
+      img: smartEyes
+    }, {
+      name: 'Star',
+      img: starEyes
+    },
+  ],
+  Hair: [
+    {
+      name: 'Default',
+      img: defaultHair
+    },
+    {
+      name: 'Bangs',
+      img: bangHair
+    },
+    {
+      name: 'Curls',
+      img: curls
+    }, {
+      name: 'Elegant',
+      img: elegant
+    }, {
+      name: 'Fancy',
+      img: fancy
+    }, {
+      name: 'Quiff',
+      img: quiff
+    }, {
+      name: 'Short',
+      img: short
+    },
+  ],
+  Legs: [
+    {
+      name: 'BubbleTea',
+      img: bubbleTea
+    }, {
+      name: 'Cookie',
+      img: cookie
+    },
+    {
+      name: 'DefaultLeg',
+      img: defaultLeg
+    }, {
+      name: 'GameConsole',
+      img: gameConsole
+    }, {
+      name: 'TiltForward',
+      img: tiltForwardLeg
+    }, {
+      name: 'tiltBackwards',
+      img: tiltBackwardLeg
+    },
+  ],
+  Mouth: [
+    {
+      name: "Astonished",
+      img: astonished
+    },
+    {
+      name: "Default",
+      img: defaultMouth
+    },
+    {
+      name: "Eating",
+      img: eating
+    },
+    {
+      name: "Laugh",
+      img: laugh
+
+    },
+    {
+      name: "Tongue",
+      img: tongue
+    }
+  ],
+  Neck: [
+    {
+      name: "Bend-Backward",
+      img: bendBackward
+    },
+    {
+      name: "Bend-Forward",
+      img: bendForward
+    },
+    {
+      name: "Default",
+      img: defaultNeck
+    },
+    {
+      name: "Thick",
+      img: thick
     }
 
+  ],
+  Nose: [
+    {
+      name: "Nose",
+      img: nose
+    }
   ]
 };
