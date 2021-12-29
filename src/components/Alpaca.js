f34479738d71e0c4a2c6bf60688b26374284424a
Auroiah Morgan <amwebdev@outlook.com>
2021-12-29 10:04:55 -0500

    Update Alpaca.js

--- src/components/Alpaca.js
+++ src/components/Alpaca.js
@@ -1,6 +1,6 @@
 export default function Alpaca(props) {
 
   return (
-    <image className="alpaca" src={props.image} alt="Your created alpaca"></image>
+    <img className="alpaca" src={props.image} alt="Your created alpaca"></img>
   );
 }
