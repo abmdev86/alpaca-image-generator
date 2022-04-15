45647a2a4485beafbbc0f78d2082943d4582df31
Auroiah Morgan <amwebdev@outlook.com>
2022-04-15 00:49:29 -0400

    Update Alpaca.js

--- src/components/Alpaca.js
+++ src/components/Alpaca.js
@@ -3,6 +3,6 @@ import React from "react";
 export default function Alpaca(props) {
 
   return (
-    <img className="alpaca" src={props.pic} alt="Your created alpaca"></img>
+    <img className="alpaca" src={props.pic} alt="Your created alpaca"/>
   );
 }
