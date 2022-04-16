34500d79f25bc783bf8073db62215ed3773c1bdb
Auroiah Morgan <amwebdev@outlook.com>
2022-04-16 13:37:53 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,4 @@
 import React from 'react';
-import { useEffect, useState } from "react";
-import { Alpaca, CategoryBtn, StyleBtn } from "./components/AlpacaComponents";
-import { styleData } from "./data/style";
 import mergeImages from "merge-images";
 import Container from "react-bootstrap/Container";
 import {CategoryButton} from "./components/ButtonComponents";
@@ -10,7 +7,7 @@ import {CategoryButton} from "./components/ButtonComponents";
 function App() {
   return (
     <Container fluid>
-      <CategoryButton />
+      <CategoryButton categoryName="Background"/>
     </Container>
   );
 }
