be69dbdff7299a72337a1abc271a7f3e7bce979e
Auroiah Morgan <amwebdev@outlook.com>
2022-04-16 21:54:04 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import mergeImages from "merge-images";
 import Container from "react-bootstrap/Container";
-import {CategoryButton} from "./components/ButtonComponents";
+import  CategoryDropDown from "./components/CategoryDropDown";
 import Styles from "./data";
 
 function App() {
   return (
     <Container fluid>
-      <CategoryButton category={Styles.Background}/>
+      <CategoryDropDown category={Styles.Background}/>
     </Container>
   );
 }
