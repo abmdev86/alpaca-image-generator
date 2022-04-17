eb172178dd209940fe0595ff25db5e8a5ccb80ae
Auroiah Morgan <amwebdev@outlook.com>
2022-04-16 22:46:16 -0400

    Update index.js

--- src/data/index.js
+++ src/data/index.js
@@ -1,6 +1,27 @@
-import { Background, Ears } from "./styles";
-const Styles ={
-Background,
-Ears
-}
+
+
+import {
+  Background,
+  Ears,
+  Eyes,
+  Hair,
+  Mouth,
+  Nose,
+  Neck,
+  Leg,
+  Accessories,
+} from "./styles";
+
+
+const Styles = {
+  Background,
+  Ears,
+  Eyes,
+  Hair,
+  Mouth,
+  Nose,
+  Neck,
+  Leg,
+  Accessories,
+};
 export default Styles;
