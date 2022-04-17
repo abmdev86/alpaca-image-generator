e490f569aa20eee13465fa49395d6c88b74a0281
Auroiah Morgan <amwebdev@outlook.com>
2022-04-16 22:46:11 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,15 @@ import Styles from "./data";
 function App() {
   return (
     <Container fluid>
-      <CategoryDropDown category={Styles.Background}/>
+      <CategoryDropDown category={Styles.Background} />
+      <CategoryDropDown category={Styles.Ears} />
+      <CategoryDropDown category={Styles.Eyes} />
+      <CategoryDropDown category={Styles.Hair} />
+      <CategoryDropDown category={Styles.Mouth} />
+      <CategoryDropDown category={Styles.Nose} />
+      <CategoryDropDown category={Styles.Neck} />
+      <CategoryDropDown category={Styles.Leg} />
+      <CategoryDropDown category={Styles.Accessories} />
     </Container>
   );
 }
