8cf89a72f03d4159b20c2a8c1269592752af50ca
Auroiah Morgan <amwebdev@outlook.com>
2023-02-21 09:16:19 -0500

    Ran build

---
+++ build/precache-manifest.5c4008961b95d5471a440b5b60f2cc53.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2092d081fc728e4b4794",
+    "url": "/static/css/main.0993ab8c.chunk.css"
+  },
+  {
+    "revision": "2092d081fc728e4b4794",
+    "url": "/static/js/main.2092d081.chunk.js"
+  },
+  {
+    "revision": "674e928106324bc9003f",
+    "url": "/static/js/1.674e9281.chunk.js"
+  },
+  {
+    "revision": "4f68f0427d5b945e45e2",
+    "url": "/static/js/2.4f68f042.chunk.js"
+  },
+  {
+    "revision": "2cbf85e9a402b6fc751b",
+    "url": "/static/js/runtime~main.2cbf85e9.js"
+  },
+  {
+    "revision": "dd28d8238f20d1aaf5ff88c1e2660772",
+    "url": "/index.html"
+  }
+];
